c9b17dd6fd82dd43b83f3d47738cc38a45cf28c6
aramrw <106574385+aramrw@users.noreply.github.com>
2024-07-08 00:02:37 -0700

    update lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -28,7 +28,35 @@ return {
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			local lspconfig = require("lspconfig")
 			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
+				on_init = function(client)
+					local path = client.workspace_folders[1].name
+					if vim.loop.fs_stat(path .. '/.luarc.json') or vim.loop.fs_stat(path .. '/.luarc.jsonc') then
+						return
+					end
+
+					client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+						runtime = {
+							-- Tell the language server which version of Lua you're using
+							-- (most likely LuaJIT in the case of Neovim)
+							version = 'LuaJIT'
+						},
+						-- Make the server aware of Neovim runtime files
+						workspace = {
+							checkThirdParty = false,
+							library = {
+								vim.env.VIMRUNTIME
+								-- Depending on the usage, you might want to add additional paths here.
+								-- "${3rd}/luv/library"
+								-- "${3rd}/busted/library",
+							}
+							-- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+							-- library = vim.api.nvim_get_runtime_file("", true)
+						}
+					})
+				end,
+				settings = {
+					Lua = {}
+				}
 			})
 			lspconfig.rust_analyzer.setup({
 				settings = {
@@ -36,9 +64,19 @@ return {
 						check = {
 							command = "clippy",
 						},
+						cargo = {
+							buildScripts = {
+								enable = true,
+							},
+						},
 						procMacro = {
 							enable = true,
 						},
+						diagnostics = {
+							useRustcErrorCode = { enable = true },
+							styleLints = { enable = true },
+							experimental = { enable = true },
+						}
 					},
 				},
 			})
@@ -58,6 +96,33 @@ return {
 			lspconfig.jsonls.setup({
 				capabilities = capabilities,
 			})
+
+			-- local border = {
+			-- 	-- TL
+			-- 	{ "┃", "FloatBorder" },
+			-- 	-- T
+			-- 	{ "▔", "FloatBorder" },
+			-- 	-- TR
+			-- 	{ "┃", "FloatBorder" },
+			-- 	-- R
+			-- 	{ "┃", "FloatBorder" },
+			-- 	-- BR
+			-- 	{ "┃", "FloatBorder" },
+			-- 	-- B
+			-- 	{ "▁", "FloatBorder" },
+			-- 	-- BL
+			-- 	{ "┃", "FloatBorder" },
+			-- 	-- L
+			-- 	{ "┃", "FloatBorder" },
+			-- }
+			--
+			-- local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+			-- function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+			-- 	opts = opts or {}
+			-- 	opts.border = opts.border or border
+			-- 	return orig_util_open_floating_preview(contents, syntax, opts, ...)
+			-- end
+			--
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
