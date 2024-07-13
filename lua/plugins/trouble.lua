f12a433e1f290b5cd869c911487efef56b8b825b
aramrw <106574385+aramrw@users.noreply.github.com>
2024-07-12 22:06:18 -0700

    better diagnostics and references for trouble

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,12 +1,40 @@
 return {
 	"folke/trouble.nvim",
 	cmd = "Trouble",
+	config = function()
+		require("trouble").setup({
+			modes = {
+				diagnostic_float = {
+					mode = "diagnostics",
+					win = {
+						type = "float",
+						relative = "editor",
+						border = "rounded",
+						title = "Diagnostics",
+						title_pos = "left",
+						size = { width = 0.8, height = 0.5 },
+						zindex = 200,
+						wrap = true,
+					},
+				},
+				references_float = {
+					mode = "lsp_references",
+					win = { type = "float" },
+				},
+			},
+			open_no_results = true,
+		})
+	end,
 	keys = {
 		{
 			"<S-m>",
-			"<cmd>Trouble diagnostics toggle focus=true win.type=float <cr>",
-			desc = "Diagnostics (Trouble)",
+			"<cmd>Trouble diagnostic_float toggle focus=true<cr>",
+			desc = "Diagnostics (Trouble)"
+		},
+		{
+			"<leader>gr",
+			"<cmd>Trouble references_float toggle focus=true<cr>",
+			desc = "References (Trouble)"
 		},
 	},
-	opts = {},
 }
