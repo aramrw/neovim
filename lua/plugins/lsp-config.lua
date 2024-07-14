c60aab30b1151093f22ef4e42187f20c7e61015c
aramrw <106574385+aramrw@users.noreply.github.com>
2024-07-14 14:50:22 -0700

    cleanup lsp-config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -51,10 +51,10 @@ return {
 			})
 			require("lspconfig").clangd.setup({
 				capabilities = capabilities,
-				cmd = {
-					"clangd",
-					"--offset-encoding=utf-16",
-				},
+				-- cmd = {
+				-- 	"clangd",
+				-- 	"--offset-encoding=utf-16",
+				-- },
 			})
 			lspconfig.tailwindcss.setup({
 				capabilities = capabilities,
@@ -75,20 +75,11 @@ return {
 
 					client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
 						runtime = {
-							-- Tell the language server which version of Lua you're using
-							-- (most likely LuaJIT in the case of Neovim)
 							version = 'LuaJIT'
 						},
 						-- Make the server aware of Neovim runtime files
 						workspace = {
 							checkThirdParty = false,
-							-- library = {
-							-- 	vim.env.VIMRUNTIME
-							-- 	-- Depending on the usage, you might want to add additional paths here.
-							-- 	-- "${3rd}/luv/library"
-							-- 	-- "${3rd}/busted/library",
-							-- }
-							-- or pull in all of 'runtimepath'. NOTE: this is a lot slower
 							library = vim.api.nvim_get_runtime_file("", true)
 						}
 					})
@@ -97,8 +88,6 @@ return {
 					Lua = {}
 				}
 			})
-
-
 			-- vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			-- vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
