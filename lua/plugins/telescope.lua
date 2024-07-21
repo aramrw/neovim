ffff593890cd4a3d2def742d483f7d4997bf1cf3
aramrw <106574385+aramrw@users.noreply.github.com>
2024-07-20 21:30:17 -0700

    update telescope layout to horizontal

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,6 +3,20 @@ return {
 	tag = "0.1.6",
 	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
+		require("telescope").setup({
+			defaults = {
+				layout_strategy = "vertical",
+				layout_config = {
+					vertical = {
+						width = 0.9,
+						height = 0.9,
+						preview_height = 0.6,
+						preview_cutoff = 0,
+						wrap_results = true
+					}
+				},
+			}
+		})
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<C-p>", builtin.find_files, {})
 		vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
