a1a142459339550a48208b009cf5321ef4b46a2a
aramrw <106574385+aramrw@users.noreply.github.com>
2024-07-26 17:11:15 -0700

    Update telescope.lua

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -18,11 +18,11 @@ return {
 			}
 		})
 		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-		vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+		-- vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+		-- vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 		-- vim.keymap.set("n", "<leader>fd", builtin.diagnostics, {})
-		vim.keymap.set("n", "<leader>fd", function()
-			builtin.diagnostics({ bufnr = 0 })
-		end)
+		-- vim.keymap.set("n", "<leader>fd", function()
+		-- 	builtin.diagnostics({ bufnr = 0 })
+		-- end)
 	end,
 }
