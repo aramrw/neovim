fd1cc459157fc8e0b48443fa49b4f747ba38bc09
aramrw <106574385+aramrw@users.noreply.github.com>
2024-08-28 17:56:02 -0700

    Update comment.lua

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,7 +1,20 @@
 return {
-	'numToStr/Comment.nvim',
-		lazy = false,
-	config = function()
-		require("Comment").setup()
-	end
+	{
+		"JoosepAlviste/nvim-ts-context-commentstring",
+		lazy = true,
+		opts = {
+			enable_autocmd = false,
+		},
+	},
+	{
+		"echasnovski/mini.comment",
+		event = "VeryLazy",
+		opts = {
+			options = {
+				custom_commentstring = function()
+					return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
+				end,
+			},
+		},
+	}
 }
