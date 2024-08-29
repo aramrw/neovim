3a3c354fcc16a1544b50c9a62d9c62a701e7cd64
aramrw <106574385+aramrw@users.noreply.github.com>
2024-08-28 19:33:46 -0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -12,7 +12,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local opts = {}
+-- local opts = {}
 
 require("vim-options")
 require("lazy").setup("plugins")
