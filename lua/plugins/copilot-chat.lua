5a3d12a5ffb5f9463c7f1d2eca4a8b64ae786428
aramrw <106574385+aramrw@users.noreply.github.com>
2024-08-28 17:55:45 -0700

    Update copilot-chat.lua

--- lua/plugins/copilot-chat.lua
+++ lua/plugins/copilot-chat.lua
@@ -1,23 +1,23 @@
 return {
-	"CopilotC-Nvim/CopilotChat.nvim",
-	branch = "canary",
-	dependencies = {
-		{ "github/copilot.vim" }, -- or github/copilot.vim
-		{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
-	},
-	config = function()
-		require("CopilotChat").setup({
-			context = "buffers",
-			window = {
-				layout = "float",
-				height = 0.8,
-				width = 0.8,
-			},
-		})
-
-		-- Use a function for the keymapping
-		vim.keymap.set("v", "<leader>mo", ":CopilotChat ", {})
-		vim.keymap.set("n", "<leader>mo", ":CopilotChat ", {})
-		vim.keymap.set("n", "<leader>mc", ":CopilotChatToggle<CR>", {})
-	end,
+	-- "CopilotC-Nvim/CopilotChat.nvim",
+	-- branch = "canary",
+	-- dependencies = {
+	-- 	{ "github/copilot.vim" }, -- or github/copilot.vim
+	-- 	{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+	-- },
+	-- config = function()
+	-- 	require("CopilotChat").setup({
+	-- 		context = "buffers",
+	-- 		window = {
+	-- 			layout = "float",
+	-- 			height = 0.8,
+	-- 			width = 0.8,
+	-- 		},
+	-- 	})
+	--
+	-- 	-- Use a function for the keymapping
+	-- 	vim.keymap.set("v", "<leader>mo", ":CopilotChat ", {})
+	-- 	vim.keymap.set("n", "<leader>mo", ":CopilotChat ", {})
+	-- 	vim.keymap.set("n", "<leader>mc", ":CopilotChatToggle<CR>", {})
+	-- end,
 }
