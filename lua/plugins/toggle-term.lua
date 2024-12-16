50577babd9b5c8627c372605ecf51a05c8cf0541
aramrw <106574385+aramrw@users.noreply.github.com>
2024-12-16 12:58:17 -0800

    Update toggle-term.lua

--- lua/plugins/toggle-term.lua
+++ lua/plugins/toggle-term.lua
@@ -1,56 +1,39 @@
--- return {
--- 	"akinsho/toggleterm.nvim",
--- 	version = "*",
--- 	config = function()
--- 		local powershell_options = {
--- 			shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell",
--- 			shellcmdflag =
--- 			"-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
--- 			shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
--- 			shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
--- 			shellquote = "",
--- 			shellxquote = "",
--- 		}
---
--- 		for option, value in pairs(powershell_options) do
--- 			vim.opt[option] = value
--- 		end
--- 		require("toggleterm").setup({
--- 			open_mapping = [[<C-l>]],
--- 			autochdir = true,
--- 			auto_scroll = false,
--- 			direction = "float",
--- 			shading_facto = "-50",
--- 			-- shading_ratio = '',
--- 			float_opts = {
--- 				border = "curved",
--- 				title_pos = "center",
--- 				width = 90,
--- 				height = 90,
--- 				zindex = 500,
--- 			}
--- 		})
--- 		vim.keymap.set("n", "<leader>tt", ":2ToggleTerm<CR>", {})
--- 	end,
--- }
-
--- uncomment this if on linux/wsl
--- and then comment out the above return block
-
 return {
 	"akinsho/toggleterm.nvim",
 	version = "*",
 	config = function()
-		config = require("toggleterm").setup({
+		local os_info = vim.loop.os_uname()
+
+		-- Check if the OS is Windows
+		if os_info.version:find("Windows") then
+			local powershell_options = {
+				shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell",
+				shellcmdflag =
+				"-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+				shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+				shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+				shellquote = "",
+				shellxquote = "",
+			}
+
+			for option, value in pairs(powershell_options) do
+				vim.opt[option] = value
+			end
+		end
+		require("toggleterm").setup({
 			open_mapping = [[<C-l>]],
+			autochdir = true,
+			auto_scroll = false,
+			direction = "float",
+			shading_facto = "-50",
+			-- shading_ratio = '',
 			float_opts = {
 				border = "curved",
 				title_pos = "center",
-				width = 90,
+				width = 140,
 				height = 90,
 				zindex = 500,
-			},
-			autochdir = true,
+			}
 		})
 		vim.keymap.set("n", "<leader>tt", ":2ToggleTerm<CR>", {})
 	end,
