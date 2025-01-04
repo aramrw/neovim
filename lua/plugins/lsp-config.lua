ee1dbea1aadc8bd1e795834355c546c9bac44b82
aramrw <106574385+aramrw@users.noreply.github.com>
2025-01-04 04:21:15 -0800

    Update lsp-config.lua

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -2,10 +2,10 @@ return {
 	{
 		"williamboman/mason.nvim",
 		config = function()
-			require("mason").setup(
-				{ PATH = "append" }
-			)
-		end
+			require("mason").setup({
+				PATH = "append",
+			})
+		end,
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
@@ -13,10 +13,7 @@ return {
 			require("mason-lspconfig").setup({
 				ensure_installed = {
 					"lua_ls",
-					"tailwindcss",
-					"emmet_language_server",
 					"jsonls",
-					"taplo",
 				},
 			})
 		end,
@@ -26,20 +23,17 @@ return {
 		lazy = false,
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local os_info = vim.loop.os_uname()
 			local lspconfig = require("lspconfig")
 
-			-- Function to get command configuration based on OS
-			local function get_cmd_config(server_name)
+			local os_info = vim.loop.os_uname()
+
+			local function get_cmd_config(server_name, additional_args)
 				local is_windows = os_info.sysname:lower():find("windows") ~= nil
 				local base_commands = {
 					lua_ls = "lua-language-server",
-					emmet_language_server = "emmet-language-server --stdio",
-					jsonls = "vscode-json-language-server --stdio",
-					html = "vscode-html-language-server --stdio",
-					taplo = "taplo lsp stdio",
-					-- pyright = "pyright-langserver --stdio",
-					-- pylsp = "pylsp",
+					jsonls = "vscode-json-language-server",
+					html = "vscode-html-language-server",
+					taplo = "taplo",
 				}
 
 				if not is_windows or not base_commands[server_name] then
@@ -47,25 +41,34 @@ return {
 				end
 
 				local bin_path = "c:/users/arami/appData/local/nvim-data/mason/bin/"
-				return { bin_path .. base_commands[server_name] }
+				local command = base_commands[server_name]
+
+				local cmd_parts = vim.split(command, " ")
+				local executable = bin_path .. cmd_parts[1]
+				table.remove(cmd_parts, 1)
+				table.insert(cmd_parts, 1, executable)
+
+				if additional_args and type(additional_args) == "table" then
+					vim.list_extend(cmd_parts, additional_args)
+				end
+
+				return cmd_parts
 			end
 
-			-- Helper function to setup LSP with conditional cmd
 			local function setup_lsp(server_name, extra_config)
-				local config = vim.tbl_deep_extend("force",
-					{ capabilities = capabilities },
-					extra_config or {}
-				)
+				local default_config = { capabilities = capabilities }
+				local config = vim.tbl_deep_extend("force", default_config, extra_config or {})
 
-				local cmd_config = get_cmd_config(server_name)
-				if cmd_config then
-					config.cmd = cmd_config
+				if not config.cmd then
+					local cmd_config = get_cmd_config(server_name, config.cmd_args)
+					if cmd_config then
+						config.cmd = cmd_config
+					end
 				end
 
 				lspconfig[server_name].setup(config)
 			end
 
-			-- Setup each LSP with the helper function
 			setup_lsp("lua_ls")
 			setup_lsp("rust_analyzer", {
 				settings = {
@@ -80,14 +83,18 @@ return {
 							"-Dclippy::complexity",
 							"-Wclippy::perf",
 							"-Wclippy::pedantic",
+							"-Wclippy::all",
+							"-Wclippy::cargo",
 							"-Wclippy::nursery",
+							"-Wclippy::style",
+							"-Wclippy::suspicious",
 						},
 						procMacro = {
 							enable = true,
 						},
 						diagnostics = {
 							styleLints = { enable = true },
-						}
+						},
 					},
 				},
 			})
@@ -95,16 +102,20 @@ return {
 			setup_lsp("clangd")
 			setup_lsp("tailwindcss")
 			setup_lsp("emmet_language_server")
-			setup_lsp("jsonls")
+			setup_lsp("jsonls", {
+				cmd_args = { "--stdio" }
+			})
 			setup_lsp("html")
-			setup_lsp("taplo")
+			setup_lsp("taplo", {
+				cmd_args = { "lsp", "stdio" },
+			})
 
 			-- Keybinds
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
 			vim.keymap.set({ "n" }, "<leader>gf", vim.lsp.buf.format, {})
 			vim.keymap.set("n", "<leader>gr", function()
-				require('telescope.builtin').lsp_references()
+				require("telescope.builtin").lsp_references()
 			end, {})
 		end,
 	},
