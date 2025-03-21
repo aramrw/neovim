3f405c3468ef5f06d3733b606b401567776f33e7
aramrw <106574385+aramrw@users.noreply.github.com>
2025-03-21 03:23:13 -0700

    Update toggle-term.lua

--- lua/plugins/toggle-term.lua
+++ lua/plugins/toggle-term.lua
@@ -4,22 +4,19 @@ return {
 	config = function()
 		local plat = vim.loop.os_uname().sysname;
 
-		local options = {
-			shell = plat == "Windows_NT" and "c:/users/arami/appdata/local/programs/nu/bin/nu.exe" or
-			"/opt/homebrew/bin/nu",
-			shellcmdflag = '--stdin --no-newline -c',
-			shellredir = "out+err> %s",
-			shellpipe = "| complete | update stderr { ansi strip } | tee { get stderr | save --force --raw %s } | into record",
-			shelltemp = false,
-			shellxescape = "",
-			shellxquote = "",
+		local powershell_options = {
+			shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell",
+			shellcmdflag =
+			"-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+			shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+			shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
 			shellquote = "",
+			shellxquote = "",
 		}
 
-		for opt, val in pairs(options) do
-			vim.opt[opt] = val
+		for option, value in pairs(powershell_options) do
+			vim.opt[option] = value
 		end
-
 
 		require("toggleterm").setup({
 			open_mapping = [[<C-l>]],
