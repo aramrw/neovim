330873dd94ac7093657628b40e8d27209ffbf17e
aramrw <106574385+aramrw@users.noreply.github.com>
2025-06-02 06:14:10 -0700

    update

--- lua/plugins/color-schemes.lua
+++ lua/plugins/color-schemes.lua
@@ -135,6 +135,19 @@ local colorschemes = {
 		"metalelf0/jellybeans-nvim",
 		dependencies = "rktjmp/lush.nvim",
 		lazy = false,
+	},
+	{
+		"metalelf0/black-metal-theme-neovim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("black-metal").setup({
+				-- Can be one of: bathory | burzum | dark-funeral | darkthrone | emperor | gorgoroth | immortal | impaled-nazarene | khold | marduk | mayhem | nile | taake | thyrfing | venom | windir
+				theme = "impaled-nazarene",
+				-- Can be one of: 'light' | 'dark', or set via vim.o.background
+				variant = "dark",
+			})
+		end,
 	}
 }
 
