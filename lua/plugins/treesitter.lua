fbbca102233e9bb735b1d47d42fe917879bc1224
aramrw <106574385+aramrw@users.noreply.github.com>
2025-06-25 19:52:27 -0700

    update shell and treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,12 +2,34 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	config = function()
-	local config = require("nvim-treesitter.configs")
-	config.setup({
-			ensure_installed = { "lua", "javascript", "typescript", "rust" },
+		-- Explicitly tell Neovim how to identify .slint files.
+		-- This is often the missing piece of the puzzle.
+		vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+			pattern = "*.slint",
+			callback = function()
+				vim.bo.filetype = "slint"
+			end,
+		})
+
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			ensure_installed = { "lua", "javascript", "typescript", "rust", "slint" },
 			auto_install = true,
 			highlight = { enable = true },
 			indent = { enable = true },
+
+			-- This section tells nvim-treesitter where to get the Slint parser from.
+			parser_configs = {
+				slint = {
+					install_info = {
+						url = "https://github.com/slint-ui/tree-sitter-slint",
+						files = {"src/parser.c"},
+						branch = "main",
+					},
+					filetype = "slint",
+				},
+			},
 		})
 	end
 }
+
