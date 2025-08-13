f2df650121c52869003ae59f248ab70a64104dc8
aramrw <aramitsa@gmail.com>
2025-08-13 02:03:41 +0000

    add rhai file types

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,35 +1,21 @@
+-- In your lazy.nvim plugins file
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	config = function()
-		-- Explicitly tell Neovim how to identify .slint files.
-		-- This is often the missing piece of the puzzle.
-		vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-			pattern = "*.slint",
-			callback = function()
-				vim.bo.filetype = "slint"
-			end,
-		})
-
-		local config = require("nvim-treesitter.configs")
-		config.setup({
-			ensure_installed = { "lua", "javascript", "typescript", "rust", "slint" },
+		-- Your standard nvim-treesitter setup.
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = { "lua", "rust" },
 			auto_install = true,
 			highlight = { enable = true },
 			indent = { enable = true },
-
-			-- This section tells nvim-treesitter where to get the Slint parser from.
-			parser_configs = {
-				slint = {
-					install_info = {
-						url = "https://github.com/slint-ui/tree-sitter-slint",
-						files = {"src/parser.c"},
-						branch = "main",
-					},
-					filetype = "slint",
-				},
-			},
 		})
-	end
+		-- vim.treesitter.language.register("rust", "rhai")
+		vim.filetype.add {
+			extension = {
+				rhai = "rhai",
+			}
+		}
+		vim.treesitter.language.register("rust", "rhai")
+	end,
 }
-
