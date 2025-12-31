f27ab431f5d6830b8f5f3e84382eca7441e3fff8
aramrw <aramitsa@gmail.com>
2025-12-31 17:37:57 +0000

    fixing the damage

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,59 +1,67 @@
 return {
-	{
+	"neovim/nvim-lspconfig",
+	dependencies = {
 		"mason-org/mason.nvim",
-		opts = {},
-	},
-
-	{
 		"williamboman/mason-lspconfig.nvim",
-		dependencies = { "mason-org/mason.nvim" },
-		opts = {
-			-- this is enabled by default
-			automatic_enable = false,
-			ensure_installed = { "lua_ls" },
-		},
 	},
+	config = function()
+		-- 1. Setup Mason
+		require("mason").setup()
 
-	-- 3. nvim-lspconfig, with the critical addition.
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			-- This defines global LSP behavior (keymaps, etc.)
-			local on_attach = function(client, bufnr)
-				-- This on_attach will apply to lua_ls and any other non-Rust LSPs.
-				local opts = { noremap = true, silent = true, buffer = bufnr }
+		-- 2. Setup Mason-LSPConfig (v2.0 Syntax)
+		require("mason-lspconfig").setup({
+			ensure_installed = {
+				"lua_ls",
+				"clangd",
+				"gopls",
+				"taplo",
+				"rust_analyzer",
+			},
+			-- NEW: 'handlers' is now a field inside setup(), not a separate function.
+			handlers = {
+				-- The default handler (applies to servers not listed below)
+				function(server_name)
+					require("lspconfig")[server_name].setup({})
+				end,
+
+				-- Custom handler for Lua
+				["lua_ls"] = function()
+					require("lspconfig").lua_ls.setup({
+						settings = {
+							Lua = {
+								diagnostics = { globals = { "vim" } },
+							},
+						},
+					})
+				end,
+
+				-- Custom handler for Rust
+				["rust_analyzer"] = function()
+					require("lspconfig").rust_analyzer.setup({
+						settings = {
+							["rust-analyzer"] = {
+								cargo = {
+									extraEnv = { RUSTUP_TOOLCHAIN = "nightly" },
+									features = "all",
+								},
+								procMacro = { enable = true },
+							},
+						},
+					})
+				end,
+			},
+		})
+
+		-- 3. Global Keymaps (LspAttach)
+		vim.api.nvim_create_autocmd("LspAttach", {
+			group = vim.api.nvim_create_augroup("UserLspConfig", { clear = true }),
+			callback = function(ev)
+				local opts = { buffer = ev.buf, silent = true }
 				vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
 				vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, opts)
 				vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
 				vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, opts)
-			end
-			-- Set up a global autocommand for the on_attach function.
-			vim.api.nvim_create_autocmd('LspAttach', {
-				group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-				callback = function(ev)
-					-- Ensure the attached LSP is not rust-analyzer before applying general keymaps
-					local client = vim.lsp.get_client_by_id(ev.data.client_id)
-					--
-					on_attach(client, ev.buf)
-				end,
-			})
-			require("lspconfig").clangd.setup({})
-			require("lspconfig").gopls.setup({})
-			require("lspconfig").taplo.setup({})
-			require("lspconfig").rust_analyzer.setup({
-				settings = {
-					["rust-analyzer"] = {
-						-- check = { command = "clippy" },
-						cargo = {
-							extraEnv = { RUSTUP_TOOLCHAIN = "nightly" },
-							features = "all",
-						},
-						procMacro = { enable = true },
-					},
-				},
-			})
-			-- Now, manually set up the servers we DO want lspconfig to manage.
-			require("lspconfig").lua_ls.setup({})
-		end,
-	},
+			end,
+		})
+	end,
 }
